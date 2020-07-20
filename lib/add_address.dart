c4edb395d738a94183807b6db9a0c79338c96b66
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-20 14:21:07 +0530

    addAddressDone

--- lib/add_address.dart
+++ lib/add_address.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AddAddress extends StatelessWidget {
+  final TextEditingController pinCodeController = TextEditingController();
+  final TextEditingController houseController = TextEditingController();
+  final TextEditingController roadController = TextEditingController();
+  final TextEditingController cityController = TextEditingController();
+  final TextEditingController stateController = TextEditingController();
+  final TextEditingController landmarkController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController mobileController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,100 +20,131 @@ class AddAddress extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Text(
-                'Add Your Address',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(
+                  'Add Your Address',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(labelText: 'Pincode'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: 'Pincode'),
+                  controller: pinCodeController,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration:
-                    InputDecoration(labelText: 'House No, Building Name '),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration:
+                      InputDecoration(labelText: 'House No, Building Name '),
+                  controller: houseController,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration:
-                    InputDecoration(labelText: 'Road Name, Area, Colony'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration:
+                      InputDecoration(labelText: 'Road Name, Area, Colony'),
+                  controller: roadController,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 170,
-                      child: TextFormField(
-                        decoration: InputDecoration(labelText: 'City'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 170,
+                        child: TextFormField(
+                          decoration: InputDecoration(labelText: 'City'),
+                          controller: cityController,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 215,
-                      child: TextFormField(
-                        decoration: InputDecoration(labelText: 'State'),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 215,
+                        child: TextFormField(
+                          decoration: InputDecoration(labelText: 'State'),
+                          controller: stateController,
+                        ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(labelText: 'Landmark (Optional)'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: 'Landmark (Optional)'),
+                  controller: landmarkController,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(labelText: 'Name'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: 'Name'),
+                  controller: nameController,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(labelText: 'Mobile'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: 'Mobile'),
+                  controller: mobileController,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 50),
-              child: Container(
-                  width: 300,
-                  height: 50,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    onPressed: () {},
-                    color: Color(0xFFEDA89D),
-                    textColor: Colors.white,
-                    child: Text("Save".toUpperCase(),
-                        style: TextStyle(fontSize: 14)),
-                  )),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: Container(
+                    width: 300,
+                    height: 50,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      onPressed: () {
+                        addStringToSF();
+                      },
+                      color: Color(0xFFEDA89D),
+                      textColor: Colors.white,
+                      child: Text("Save".toUpperCase(),
+                          style: TextStyle(fontSize: 14)),
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  addStringToSF() async {
+    String name = nameController.text;
+    String address = houseController.text;
+    String city = cityController.text;
+    String state = stateController.text;
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    prefs.setString('name', name);
+    prefs.setString('address', address);
+    prefs.setString('city', city);
+    prefs.setString('state', state);
+
+    print(name);
+    print(address);
+    print(city);
+    print(state);
   }
 }
--- lib/address_page.dart
+++ lib/address_page.dart
@@ -1,8 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:registration/add_address.dart';
 
-class AddressPage extends StatelessWidget {
+class AddressPage extends StatefulWidget {
+  String name;
+  String address;
+  @override
+  _AddressPageState createState() => _AddressPageState();
+}
+
+class _AddressPageState extends State<AddressPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Address'),
+        backgroundColor: Color(0xFFEDA89D),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(3.0),
+              child: Container(
+                height: 200,
+                child: Card(
+                  color: Color(0xFFEDA89D),
+                  child: Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        FittedBox(
+                          child: Text(
+                            'Name : ${widget.name}',
+                            style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                        FittedBox(
+                          child: Text(
+                            'Address : ${widget.address}',
+                            style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+          itemCount: 2,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFFEDA89D),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddAddress()));
+        },
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
